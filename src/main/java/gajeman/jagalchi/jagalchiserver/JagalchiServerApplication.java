cce5b32505e09e38cf036aa2a062d9ae5ceb7cb2
leegunwoo <leegunwoo0325@gmail.com>
2025-12-12 00:04:24 +0900

    initial commit

---
+++ src/main/java/gajeman/jagalchi/jagalchiserver/JagalchiServerApplication.java
@@ -0,0 +1,13 @@
+package gajeman.jagalchi.jagalchiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JagalchiServerApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(JagalchiServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gajeman/jagalchi/jagalchiserver/JagalchiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package gajeman.jagalchi.jagalchiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JagalchiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
